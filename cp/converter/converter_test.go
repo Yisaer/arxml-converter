da2aa354d5fdadf27afdd190a00f64241e26f75d
Song Gao <disxiaofei@163.com>
2025-10-13 14:40:35 +0800

    fix cp converter
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- cp/converter/converter_test.go
+++ cp/converter/converter_test.go
@@ -1,7 +1,6 @@
 package converter
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,5 +12,14 @@ func TestGetDataType(t *testing.T) {
 	require.NoError(t, err)
 	tr, err := c.GetDataTypeByID(33282, 2181169157)
 	require.NoError(t, err)
-	fmt.Println(tr.TypeName())
+	require.Equal(t, "string", tr.TypeName())
+	testData := []byte{
+		0x00, 0x00, 0x00, 0x08, // 长度字段 (8字节)
+		0xEF, 0xBB, 0xBF, // UTF-8 BOM
+		0x54, 0x65, 0x73, 0x74, // "Test"
+		0x00, // UTF-8 终止符
+	}
+	v, err := c.Convert(33282, 2181169157, testData)
+	require.NoError(t, err)
+	require.Equal(t, "Test", v)
 }
